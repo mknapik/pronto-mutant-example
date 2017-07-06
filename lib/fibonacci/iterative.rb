9649181e996036e0e5d375fcc9a041d6f2c3f08f
Michał Knapik <michal.knapik@u2i.com>
2017-07-06 17:18:22 +0200

    Break rubocop

--- lib/fibonacci/iterative.rb
+++ lib/fibonacci/iterative.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 module Fibonacci
-  class Iterative
-    class Boo
-      class Dummy
-      end
-    end
-
-    def self.call(n)
-      result = 1
-      previous = 1
-
-      (2..n).each do
-        result, previous = previous + result, result
+    class Iterative
+      class Boo
+        class Dummy
+        end
       end
 
-      result
+      def self.call(n)
+        result = 1
+        previous = 1
+
+        (2..n).each do
+          result, previous = previous + result, result
+        end
+
+        result
+      end
     end
-  end
 end
